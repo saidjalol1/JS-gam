2ee63237c32f17520c9951d1d34d7788b4d3eb6b
saidjalol1908@gmail.com <turakhujayevsaidjalol06@gmail.com>
2023-04-25 16:15:09 +0500

    JavaScript game add

---
+++ JavaScript/script.js
@@ -0,0 +1,41 @@
+let elForm = document.querySelector("#form-js");
+let elInput = elForm.querySelector("#input-js");
+let elSend = elForm.querySelector("#input-btn");
+let elChance = document.querySelector("#chance");
+let elRefresh = document.querySelector("#refresh");
+let elResult = document.querySelector("#result");
+let random = Math.round(Math.random() * 101);
+
+elChance.textContent = 5
+elForm.addEventListener("submit" , (evt) =>{
+    evt.preventDefault();
+    
+    let strton = Number(elInput.value);
+    let chance = elChance.textContent--
+     
+     if(chance === 0 || chance < 2){
+        elSend.disabled = true;
+        elInput.disabled = true;
+        elResult.textContent = `You have spent all your chances, The Number was ${random}`;
+     }
+     else if (strton > 100 || strton < 0){
+        elResult.textContent = "TITLE ni o'qing yahshilab";
+     }
+     else if (random === strton ){
+        elResult.textContent = `topdingiz rostandham ${strton} edi , Kundaligizi apkesez 5 qo'yberaman`;
+        elSend.disabled = true;
+        elInput.disabled = true;
+     }
+     else if (random > strton){
+        elResult.textContent = "SIZ kiritgan son computer o'ylagandan kakichkina";
+     }
+     else if (random < strton){
+        elResult.textContent = "SIZ kiritgan son computer o'ylagandan katta";
+     }
+     
+    
+})
+
+elRefresh.addEventListener("click", ()=>{
+    window.location.reload()
+})
